d0e8d088f9b1f33a9daf7660493a7f1fe1744938
Choo <choo000407@naver.com>
2023-11-15 21:29:00 +0900

    feat(Controller): MVP를 컨트롤러로 대체한다

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,33 +1,11 @@
 package christmas;
 
-import christmas.model.event.Event;
-import christmas.model.event.dto.EventResult;
-import christmas.model.order.Order;
-import christmas.model.order.dto.OrderRequest;
-import christmas.model.order.dto.OrderResult;
+import christmas.controller.EventPlannerController;
 import christmas.view.InputView;
 import christmas.view.OutputView;
-import java.time.LocalDate;
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // MVP 구현
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        Order order = new Order();
-        Event event = new Event();
-
-        outputView.printEventPlannerStart();
-        LocalDate date = inputView.readDate();
-
-        List<OrderRequest> orderRequests = inputView.readOrderRequests();
-        order.markMenusBy(orderRequests);
-        OrderResult orderResult = order.createOrderResult();
-        outputView.printEventBenefitStartWith(date);
-        outputView.printOrderResult(orderResult);
-
-        EventResult eventResult = event.applyTo(order, date);
-        outputView.printEventResult(eventResult);
+        new EventPlannerController(new InputView(), new OutputView()).plan();
     }
 }
--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -5,36 +5,44 @@ import christmas.model.event.dto.EventResult;
 import christmas.model.order.Order;
 import christmas.model.order.dto.OrderRequest;
 import christmas.model.order.dto.OrderResult;
+import christmas.view.InputView;
+import christmas.view.OutputView;
 import java.time.LocalDate;
 import java.util.List;
 
 public class EventPlannerController {
+    private final InputView inputView;
+    private final OutputView outputView;
     private final Order order;
     private final Event event;
 
-    public EventPlannerController() {
+    public EventPlannerController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
         this.order = new Order();
         this.event = new Event();
     }
 
     public void plan() {
         LocalDate visitDate = planDate();
-        planOrder();
+        planOrder(visitDate);
         applyEventToOrder(visitDate);
     }
 
     private LocalDate planDate() {
-        return LocalDate.now(); // TODO : 입력값으로 교체하기
+        outputView.printEventPlannerStart();
+        return inputView.readDate();
     }
 
-    private void planOrder() {
-        List<OrderRequest> orderRequests = List.of(); // TODO : 입력값으로 교체하기
+    private void planOrder(LocalDate visitDate) {
+        List<OrderRequest> orderRequests = inputView.readOrderRequests();
         OrderResult orderResult = order.markMenusBy(orderRequests);
-        // TODO : 출력하기
+        outputView.printEventBenefitStartWith(visitDate);
+        outputView.printOrderResult(orderResult);
     }
 
     private void applyEventToOrder(LocalDate visitDate) {
         EventResult eventResult = event.applyTo(order, visitDate);
-        // TODO : 출력하기
+        outputView.printEventResult(eventResult);
     }
 }
