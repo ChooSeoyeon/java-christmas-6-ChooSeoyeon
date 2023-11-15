4abf1e3a1972262344ae2fce43cb18d0fc832e15
Choo <choo000407@naver.com>
2023-11-15 19:04:49 +0900

    feat(OutputView): `이벤트 플래너 시작 문구`를 출력한다

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,9 @@
+package christmas.view;
+
+public class OutputView {
+    private static final String EVENT_PLANNER_START = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public void printEventPlannerStart() {
+        System.out.println(EVENT_PLANNER_START);
+    }
+}
