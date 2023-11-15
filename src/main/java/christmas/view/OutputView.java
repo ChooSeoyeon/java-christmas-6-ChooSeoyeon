28708f6027dc450a6bf594720b136c0893d00615
Choo <choo000407@naver.com>
2023-11-15 19:16:10 +0900

    feat(OutputView): `이벤트 혜택 미리보기 문구`를 출력한다

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,8 +2,13 @@ package christmas.view;
 
 public class OutputView {
     private static final String EVENT_PLANNER_START = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String EVENT_BENEFIT_START = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
 
     public void printEventPlannerStart() {
         System.out.println(EVENT_PLANNER_START);
+    }
+
+    public void printEventBenefitStartWith(int date) {
+        System.out.println(String.format(EVENT_BENEFIT_START, date));
     }
 }
